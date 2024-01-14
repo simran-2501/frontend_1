041d5ca94d9061b6b9ccd43dcc4f4a7245b76899
Pranav Malakar <pranav.malakar@gmail.com>
2024-01-14 19:12:04 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import BackgroundLogo from '../assets/logo_bg.png'
 import Avatar from '../assets/avatar.png'
-function Navbar({title}) {
+
+import { Link } from 'react-router-dom';
+
+function Navbar({ title }) {
+  const headerLinks = [
+    { title: 'DASHBOARD', path: '/' },
+    { title: 'Object Detection', path: '/objectdetection' },
+    { title: 'Person Detection', path: '/persondetection' },
+    { title: 'Vehicle Detection', path: '/vehicledetection' },
+    { title: 'Number Plate Detection', path: '/numberplatedetection' },
+  ];
+
   return (
     <div className='flex items-center bg-indigo-600 px-5'>
-        <img src={BackgroundLogo} alt="no_logo" width={50}/>
-        <h1 className='ml-5 font-bold text-white text-2xl'>{title}</h1>
-        <img src={Avatar} alt="no_avatar" className='ml-auto'/>
+      <img src={BackgroundLogo} alt="no_logo" width={50} className='mr-2' />
+      {headerLinks.map(({ title, path }, index) => (
+        <Link key={index} to={path} className='ml-5 text-white hover:text-gray-300 transition duration-300'>
+          <h1 className='font-bold text-2xl capitalize'>{title}</h1>
+        </Link>
+      ))}
+      <img src={Avatar} alt="no_avatar" className='ml-auto' />
     </div>
-  )
+  );
 }
+
+
 
 export default Navbar
